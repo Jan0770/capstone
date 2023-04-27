4074cc49e7efbf3616c973e8e945262616832b80
Jan0770 <jo.otten2@web.de>
2023-04-27 17:42:00 +0200

    included a route table for db subnets

--- network.tf
+++ network.tf
@@ -33,3 +33,25 @@ resource "aws_route_table_association" "host_subnet_2_association" {
     subnet_id      = aws_subnet.host_subnet_2.id
     route_table_id = aws_route_table.public_route_table.id
 }
+
+resource "aws_route_table" "db_route_table" {
+    vpc_id = aws_vpc.vpc.id
+
+    route {
+        cidr_block = "10.0.0.0/16"
+    }
+
+    tags = {
+        Name = "db route table"
+    }
+}
+
+resource "aws_route_table_association" "db_subnet_association_1" {
+    subnet_id      = aws_subnet.db_subnet_1.id
+    route_table_id = aws_route_table.db_route_table.id
+}
+
+resource "aws_route_table_association" "db_subnet_association_2" {
+    subnet_id      = aws_subnet.db_subnet_2.id
+    route_table_id = aws_route_table.db_route_table.id
+}
